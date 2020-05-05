b23b9377c6ff20ec7dad92eb4a826e1bfa6b060d
UcheSylvester <okorocode@gmail.com>
2020-05-05 10:44:20 +0100

    created a subscriber to subscribe to changes in the store

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const createStore = () => {
-  let store;
+  // The store
+  let store,
+    listeners = [];
 
+  // getting the store
   const getStore = () => store;
+
+  const subscribe = (listener) => listeners.push(listener);
 
   return {
     getStore,
   };
 };
+
+// user
+const store = createStore();
+
+store.subscribe(() => {
+  console.log("The state is:", store.getStore());
+});
+
+store.subscribe(() => {
+  console.log("new state is:", store.getStore());
+});
